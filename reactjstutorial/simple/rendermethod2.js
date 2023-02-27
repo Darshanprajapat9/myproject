b175b7fefa0f3a47cf22f4d56bbcf01256bd0814
Darshanprajapat9 <105969738+Darshanprajapat9@users.noreply.github.com>
2023-02-27 17:11:52 +0530

    Create rendermethod2.js

---
+++ reactjstutorial/simple/rendermethod2.js
@@ -0,0 +1,21 @@
+ 
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+ 
+
+ // render method 2
+
+ //const arr =['vinod','darshan','prajapat']
+
+/* 
+ ReactDOM.render(
+    [
+<h1> Hello world</h1> ,
+<p>i am learn to react js</p>,
+<h2> i am started to learn</h2>,
+    ],
+ document.getElementById('root') );
+
+ 
