426ba60c8fdbbd655274e666959c981f999c2645
Darshanprajapat9 <105969738+Darshanprajapat9@users.noreply.github.com>
2023-02-27 17:14:08 +0530

    Create fragmentmethod.js

---
+++ reactjstutorial/simple/fragmentmethod.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+ 
+//this is fragment method
+ 
+ReactDOM.render(
+    <React.Fragment>
+<h1> Hello world</h1> 
+<p>i am learn to react js</p>
+<h2> i am started to learn</h2>
+</React.Fragment>
+,
+ document.getElementById('root') );
+ 
+
+ 
+//this is challnge of one
+
+ ReactDOM.render(
+   <>
+   <h1>Darshan Prajapat</h1>
+   <p>List of 5 serise.</p>
+   <ol type="1">
+    <li> Dark</li>
+    <li> Extra curricular</li>
+    <li> My holo love</li>
+    <li> my first- love</li>
+    <li> my robote</li>
+   </ol>
+   </>,
+   document.getElementById("root"));
+  
