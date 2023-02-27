b5dba20d9d35b902d272369d7fdaba1e04e6e705
Darshanprajapat9 <105969738+Darshanprajapat9@users.noreply.github.com>
2023-02-27 17:04:37 +0530

    Create index.js

---
+++ reactjstutorial/simple/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+ //ReactDOM.render('kya dikhana he','kaha dikhana he','call back function')
+*/
+
+// render method 1
+/*
+ReactDOM.render(
+    <div>
+<h1> Hello world</h1> 
+<p>i am learn to react js</p>
+<h2> i am started to learn</h2>
+</div>
+,
+ document.getElementById('root') ); 
