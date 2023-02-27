af9eaf94bcce8043abc119a3a5e8d98900c4ee30
Darshanprajapat9 <105969738+Darshanprajapat9@users.noreply.github.com>
2023-02-27 17:53:57 +0530

    Create export.jsx

---
+++ reactjstutorial/import&export/export.jsx
@@ -0,0 +1,12 @@
+export {name , myfunction};
+
+import React from 'react';
+import add , { sub , div ,mult}from './Calculator';
+
+function App(){
+    return(<><h1>sum of two number add(40,4) :{ add(40,4)}</h1>
+    <h1>sub of two number  sub(40,4): {sub(40,4)}</h1>
+    <h1>Div of two number  div(40,4): {div(40,4)}</h1>
+    <h1>mult of two number mult(40,4) : {mult(40,4)}</h1></>);
+}
+export default App;
