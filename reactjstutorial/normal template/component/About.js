1bc98a0e849e0eb30473d02f6ad5dcc65f78ace6
Darshanprajapat9 <105969738+Darshanprajapat9@users.noreply.github.com>
2023-03-02 11:36:07 +0530

    Create About.js

---
+++ reactjstutorial/normal template/component/About.js
@@ -0,0 +1,28 @@
+aimport React from "react";
+const About = ()  =>{
+    return(
+        <div className="about">
+            <div className="container">
+             <div className="row">
+                <div className="col-6 p-25">
+                <h3>The PIzza Menu</h3>
+                <h1> CHICAGP <br/> THIN CRUST</h1>
+               <p>lorem nuhbuebvebfhugg3y8h  jb f3hun hhv u j hvubn vbhub3rj vbyuk hjbgiebnxjg3khyu nljk jbu4 j r3gjboucjh efwhubu</p>
+              <div className="about_btn">
+                <a href="#" className="btn btn-smart">Read More</a>
+              </div>
+                </div>
+
+                <div className="col-6">
+                    <div className="about__img">
+                        <img src="../img/b1.jpg"/>
+                    </div>
+                </div>
+             </div>
+            </div>
+
+
+        </div>
+    );
+}
+export default About;
