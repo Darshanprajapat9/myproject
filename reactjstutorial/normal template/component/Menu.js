289f19a8f2f7232ca37495c51f13f6ef9969dc5e
Darshanprajapat9 <105969738+Darshanprajapat9@users.noreply.github.com>
2023-03-02 11:38:42 +0530

    Create Menu.js

---
+++ reactjstutorial/normal template/component/Menu.js
@@ -0,0 +1,30 @@
+import React from "react";
+const Menu = ()  =>{
+    return(
+        <div className="about">
+            <div className="container">
+             <div className="row">
+                
+                 <div className="col-6">
+                    <div className="about__img">
+                        <img src="../img/b1.jpg"/>
+                    </div>
+                </div>
+
+                <div className="col-6 p-25">
+                <h3>About us</h3>
+                <h1> Wel Come TO Naestro Pizzini</h1>
+               <p>lorem nuhbuebvebfhugg3y8h  jb f3hun hhv u j hvubn vbhub3rj vbyuk hjbgiebnxjg3khyu nljk jbu4 j r3gjboucjh efwhubu</p>
+              <div className="about_btn">
+                <a href="#" className="btn btn-smart">View More</a>
+              </div>
+                </div>
+
+             </div>
+            </div>
+
+
+        </div>
+    );
+}
+export default Menu;
