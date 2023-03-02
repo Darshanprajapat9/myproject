12ae06150706dd4bdd2bd52fbcae204a819b78d1
Darshanprajapat9 <105969738+Darshanprajapat9@users.noreply.github.com>
2023-03-02 11:40:22 +0530

    Create Price.js

---
+++ reactjstutorial/normal template/component/Price.js
@@ -0,0 +1,60 @@
+import React from 'react';
+const Price =() =>{
+return(
+    <div className='Prices'>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-4'>
+                    <div className='price__img'>
+                       <img src="../img/b1.jpg"/>
+                    </div>
+                    <h1 className='prices__heading'>Pizza Margeritta</h1>
+                      <p>bhuvbwy7gvuhby8ru  uhwvd jb abu bj ewjfboucb hve u hk asvjn chb fuybdf cdhb</p>
+                  <p className='price_rs'>$50.00</p>
+                </div>
+                <div className='col-4'>
+                    <div className='price__img'>
+                       <img src="../img/b1.jpg"/>
+                    </div>
+                    <h1 className='prices__heading'>Pizza Margeritta</h1>
+                      <p>bhuvbwy7gvuhby8ru  uhwvd jb abu bj ewjfboucb hve u hk asvjn chb fuybdf cdhb</p>
+                  <p className='price_rs'>$50.00</p>
+                </div>
+                <div className='col-4'>
+                    <div className='price__img'>
+                       <img src="../img/b1.jpg"/>
+                    </div>
+                    <h1 className='prices__heading'>Pizza Margeritta</h1>
+                      <p>bhuvbwy7gvuhby8ru  uhwvd jb abu bj ewjfboucb hve u hk asvjn chb fuybdf cdhb</p>
+                  <p className='price_rs'>$50.00</p>
+                </div>
+                <div className='col-4'>
+                    <div className='price__img'>
+                       <img src="../img/b1.jpg"/>
+                    </div>
+                    <h1 className='prices__heading'>Pizza Margeritta</h1>
+                      <p>bhuvbwy7gvuhby8ru  uhwvd jb abu bj ewjfboucb hve u hk asvjn chb fuybdf cdhb</p>
+                  <p className='price_rs'>$50.00</p>
+                </div>
+                <div className='col-4'>
+                    <div className='price__img'>
+                       <img src="../img/b1.jpg"/>
+                    </div>
+                    <h1 className='prices__heading'>Pizza Margeritta</h1>
+                      <p>bhuvbwy7gvuhby8ru  uhwvd jb abu bj ewjfboucb hve u hk asvjn chb fuybdf cdhb</p>
+                  <p className='price_rs'>$50.00</p>
+                </div>
+                <div className='col-4'>
+                    <div className='price__img'>
+                       <img src="../img/b1.jpg"/>
+                    </div>
+                    <h1 className='prices__heading'>Pizza Margeritta</h1>
+                      <p>bhuvbwy7gvuhby8ru  uhwvd jb abu bj ewjfboucb hve u hk asvjn chb fuybdf cdhb</p>
+                  <p className='price_rs'>$50.00</p>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+}
+export default Price;
