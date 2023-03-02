e91900e2454c0041f096cace8e5e67d131b79cd2
Darshanprajapat9 <105969738+Darshanprajapat9@users.noreply.github.com>
2023-03-02 11:19:16 +0530

    Create index.js

---
+++ reactjstutorial/import&export/card/method2/index.js
@@ -0,0 +1,24 @@
+//custmam type (props metthod)
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+ import Card from './Card';
+
+ReactDOM.render(<><div className='row'>
+<Card imgsrc="https://wallpapercave.com/wp/wp4056410.jpg" 
+title=" A Netflix Orignal Series"
+sname = 'DARK'
+link="https://www.netflix.com/in/title/80990668?source=3"
+/>
+<Card imgsrc="https://wallpapercave.com/wp/wp4056410.jpg" 
+title=" A Netflix Orignal Series"
+sname = 'DARK'
+link="https://www.netflix.com/in/title/80990668?source=3"
+/>
+<Card imgsrc="https://wallpapercave.com/wp/wp4056410.jpg" 
+title=" A Netflix Orignal Series"
+sname = 'DARK'
+link="https://www.netflix.com/in/title/80990668?source=3"
+/>
+</div>
+</>,document.getElementById("root"));
