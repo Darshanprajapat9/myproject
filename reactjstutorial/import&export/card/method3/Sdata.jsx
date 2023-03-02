edb97426c205ea583132f12ed0631f42ff28eb20
Darshanprajapat9 <105969738+Darshanprajapat9@users.noreply.github.com>
2023-03-02 11:25:11 +0530

    Create Sdata.jsx

---
+++ reactjstutorial/import&export/card/method3/Sdata.jsx
@@ -0,0 +1,36 @@
+const Sdata = [
+    {imgsrc :"https://wallpapercave.com/wp/wp4056410.jpg" ,
+title:" A Netflix Orignal Series" ,
+sname :"DARK" ,
+link:"https://www.netflix.com/in/title/80990668?source=35" ,
+},
+{
+    imgsrc:"https://www.hitc.com/static/uploads/hitcn/1816/extracurricular_netflix_780_1544944.jpg" ,
+    title:" A Netflix Orignal Series" ,
+    sname :"extra curricular",
+    link:"https://www.netflix.com/in/title/80990668?source=35",
+},
+{
+    imgsrc:"https://wallpapercave.com/wp/wp1917154.jpg",
+  title:" A Netflix Orignal Series" ,
+  sname :"stranger Things",
+   link:"https://www.netflix.com/in/title/80990668?source=35"
+},
+{
+imgsrc:"https://images.justwatch.com/poster/8589251/s592.jpg",
+  title:" A Netflix Orignal Series" ,
+  sname :"The Vampire Diaries",
+   link:"https://www.netflix.com/in/title/80990668?source=35"
+},
+{
+    imgsrc:"https://wallpapercave.com/wp/wp1917154.jpg",
+  title:" A Netflix Orignal Series" ,
+  sname :"stranger Things",
+   link:"https://www.netflix.com/in/title/80990668?source=35"
+},
+
+
+
+
+];
+export default Sdata;
