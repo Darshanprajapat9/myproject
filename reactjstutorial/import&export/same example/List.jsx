545c6ac67b0e9d9accd19fc881dddc63db85b9ba
Darshanprajapat9 <105969738+Darshanprajapat9@users.noreply.github.com>
2023-03-02 11:07:45 +0530

    Create List.jsx

---
+++ reactjstutorial/import&export/same example/List.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function List(){
+    return ( <ol>
+    <li>Darshan</li>
+    <li>juli</li>
+    <li>vipul</li>
+    <li>naresh</li>
+    <li>masum</li>
+   </ol>) ;
+}
+export default List;
