4344362bb9f3a0c76833796c5a601d6347e8a9e1
Darshanprajapat9 <105969738+Darshanprajapat9@users.noreply.github.com>
2023-03-02 11:36:52 +0530

    Create Clients.js

---
+++ reactjstutorial/normal template/component/Clients.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Clients =() =>{
+return(
+    <div className='clients'>
+        <div className='.container'>
+            <div className='row'>
+                <div className='col-3'>
+                    <div className='clients'>
+                        <h1>120</h1>
+                        <p>Awesome Employee</p>
+                    </div>
+                </div>
+                <div className='col-3'>
+                    <div className='clients'>
+                        <h1>58</h1>
+                        <p>Pizza Types</p>
+                    </div>
+                </div>
+                <div className='col-3'>
+                    <div className='clients'>
+                        <h1>1458</h1>
+                        <p>Satisfy Customore</p>
+                    </div>
+                </div>
+                <div className='col-3'>
+                    <div className='clients'>
+                        <h1>2031</h1>
+                        <p>Devlivary</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+}
+export default Clients;
