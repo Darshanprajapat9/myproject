5d253a7c5e09326b0ad0b4c4dc4a6a5dd2950a80
Darshanprajapat9 <105969738+Darshanprajapat9@users.noreply.github.com>
2023-03-02 11:40:58 +0530

    Create Shop.js

---
+++ reactjstutorial/normal template/component/Shop.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Shop=()=>{
+return(
+    <div className='shop'>
+        <div className='container'>
+            <div className='shop__text'>
+                <div>
+                    <h3>Free Devlivary with <br/> pizza of the Day <br/>only 8.59 Usd</h3>
+                      <div className='shop__btn'>
+                        <a href='#' className='btn btn-smart'>Shop Now</a>
+                      </div>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+}
+export default Shop;
