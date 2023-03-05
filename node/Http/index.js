9fa26b2827d6e4e81a62ded6d4cc3e16afcbb9cb
Darshanprajapat9 <105969738+Darshanprajapat9@users.noreply.github.com>
2023-03-05 23:10:53 +0530

    Create index.js

---
+++ node/Http/index.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const server = http.createserver((req,res)=>{
+    res.end("hello guj my name is darshan prajapat');
+})
+  
+  server.listen(8000,'127.0.0.1',()=>{
+    console.log('comple the created you server no 8000')});
