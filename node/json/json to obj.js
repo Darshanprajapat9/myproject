3fe08a037aa48319a8bca112c89ec4f3e00bc7af
Darshanprajapat9 <105969738+Darshanprajapat9@users.noreply.github.com>
2023-03-06 00:29:34 +0530

    Create json to obj.js

---
+++ node/json/json to obj.js
@@ -0,0 +1,11 @@
+const bioData = {
+  
+  name:'darshan Prajapat',
+  age:20,
+  channel : 'darshan technical'
+}
+
+const objData  = JSON.parse(jsonData);
+console.log(objData.);
+
+result=>{ name: 'darshan', age: 20, channnel: 'darshan technical' }
