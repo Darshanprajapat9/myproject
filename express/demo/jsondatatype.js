f7a436ea905e1e443fc2ae78fea45032011cdfbb
Darshanprajapat9 <105969738+Darshanprajapat9@users.noreply.github.com>
2023-03-14 15:07:35 +0530

    Create jsondatatype.js

---
+++ express/demo/jsondatatype.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const nav = express();
+const port =3000;
+
+
+nav.get('/',(req,res)=>{
+    res.write('<h1>wel come to my home page</h1>');
+    res.write('<h1>wel come to my home page</h1>');
+    res.send();
+});
+
+nav.get('/about',(req,res)=>{
+    res.send('wel come to about page');
+});
+
+nav.get('/contact',(req,res)=>{
+    res.send('wel come to my contact page');
+});
+
+nav.get('/temp',(req,res)=>{
+    res.json([
+        {
+          id:1,
+          name :'Darshan'
+        },
+        {
+        id:2,
+        name :'Rashid'
+        }
+        ,
+        {
+            id:3,
+            name :'Nauman'
+        }
+
+]);
+});
+
+nav.listen(port ,'127.0.0.1', ()=>{
+    console.log('complate sucessful complate your chellnge');
+});
+
