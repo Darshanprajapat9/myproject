2b0af3d27def4e8971da23d957efc5e16738effa
Darshanprajapat9 <105969738+Darshanprajapat9@users.noreply.github.com>
2023-03-24 22:55:18 +0530

    Create api.js

---
+++ reactjstutorial/Accordion/src/component/api.js
@@ -0,0 +1,27 @@
+export const question = [{
+    id:1 , 
+    question :"what is virtual DOM? ",
+    answer:" Dom stand for document object model . The DOM  represent an html with a logical tree strucure"
+},{
+    id:2 , 
+    question :"How do you create a React-app",
+    answer:" install the create-react-app package using the command prompt or tutorial."
+},
+{
+    id:3 , 
+    question :"what is event in react",
+    answer:" an event is an action that a user or system may trigger ,such as pressing a key  a mouse click ,etc."
+},
+{
+    id:4 , 
+    question :"what are an event in reactjs",
+    answer:" synthetic events combine the response of different browser's native eventss intobAPI,ensuing the ."
+}
+,
+{
+    id:5 , 
+    question :"what are component",
+    answer:" component are the builiding blocks of any React application ,and a single app usillay consist of multiplle ."
+}
+
+]
